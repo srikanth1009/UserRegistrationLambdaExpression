defa88f3e10f8b27e47804ad001db66e6b92ba8b
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-10-12 11:11:07 +0530

    Added:User need to enter a vaild Email id

--- src/test/java/com/bridgelabz/userregistrationjunit/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistrationjunit/UserRegistrationTest.java
@@ -39,5 +39,19 @@ public class UserRegistrationTest {
         Assert.assertEquals(false, result);
 
     }
+    /**
+     * Unit test for validating email address
+     */
+    @Test
+    public void givenEmail_WhenProper_ShouldReturnTrue() {
+        boolean result = userRegistration.email("abc@gmail.com.com");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenEmail_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.email("abc()*@gmail.com");
+        Assert.assertEquals(false, result);
+    }
 }
 
